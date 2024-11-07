32d04bc7b4f08d9ee8dd558f5db6c5f4cbf1785f
Chris M <c.muetterlein@gmail.com>
2024-11-07 23:42:48 +0100

    Laravel base setup

---
+++ app/Console/Commands/TestDBConnection.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+
+class TestDBConnection extends Command
+{
+    protected $signature = 'db:test';
+    protected $description = 'Test the database connection';
+
+    public function handle()
+    {
+        try {
+            DB::connection()->getPdo();
+            
+        } catch (\Exception $e) {
+            $this->error("✕ Verbindungsfehler: " . $e->getMessage());
+        }
+    }
+}
